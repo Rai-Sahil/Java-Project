67a41d49e868e31e3ee3d518a2dad19fc444635b
Nav <navdeepnx@gmail.com>
2022-12-03 00:40:50 -0800

    test cases added

--- src/Test/java/org/example/LaserTest.java
+++ src/Test/java/org/example/LaserTest.java
@@ -2,13 +2,27 @@ package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import processing.core.PImage;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LaserTest {
 
+  Laser laser;
+  Window window;
+  boolean isFiring = true;
+  PImage bot;
+  float posX;
+  float posY;
+  int speed;
+  float dir;
+  float size;
+
     @BeforeEach
     void setUp() {
+      window = new Window();
+      laser = new Laser(posX, posY, dir, window);
+      speed = 5;
     }
 
     @Test
@@ -22,4 +36,5 @@ class LaserTest {
     @Test
     void checkCollision() {
     }
+
 }
--- src/Test/java/org/example/StarTest.java
+++ src/Test/java/org/example/StarTest.java
@@ -2,13 +2,29 @@ package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import processing.core.PVector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StarTest {
 
+  Star star;
+  Window window;
+  public static final int max_weight = 7;
+  PVector pos;
+  public boolean isMoving = true;
+  public boolean isNotMoving = true;
+  float size;
+  float colour;
+  int margin = 50;
+  float speed;
+  int TOP_SPEED = 1;
+
     @BeforeEach
     void setUp() {
+      window = new Window();
+      star = new Star(window);
+      pos = new PVector(window.width / 2f, window.height / 2f);
     }
 
     @Test
@@ -18,4 +34,48 @@ class StarTest {
     @Test
     void move() {
     }
+
+    @Test
+    void checkStarsPosXMoving(){
+      if (pos.x > window.width + margin){
+        assertTrue(isMoving);
+      }
+    }
+
+  @Test
+  void checkStarsPosXNotMoving(){
+    if (pos.x < window.width + margin){
+      assertTrue(isNotMoving);
+    }
+  }
+
+    @Test
+    void checkStarsPosYMoving(){
+      if (pos.y > window.height + margin){
+        assertTrue(isMoving);
+      }
+    }
+
+    @Test
+    void checkStarsPosYNotMoving(){
+      if (pos.y < window.height + margin){
+        assertTrue(isNotMoving);
+      }
+    }
+
+    @Test
+    void checkStarSpeed(){
+      if (speed == window.map(size, 0, max_weight, 1, TOP_SPEED)){
+        assertTrue(isMoving);
+      }
+    }
+
+    @Test
+    void checkStarNoSpeed(){
+      if (speed != window.map(size, 0, max_weight, 1, TOP_SPEED)){
+        assertTrue(isNotMoving);
+      }
+    }
+
+
 }
